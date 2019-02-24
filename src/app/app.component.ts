7f9d2505b609c8d18e300b936f8a2487357456ca
Sreedhar <sreedhar85in@gmail.com>
2019-02-24 22:55:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'poojastest';
+  uname;
+constructor()
+{
+  
+}
+  ngOnInit()
+  {
+  
+  }
+
+  submit()
+  {
+    this.uname=null;
+  }
 }
